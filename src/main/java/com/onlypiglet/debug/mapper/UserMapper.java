e0a36682e761d835cad4e724af94117b4b74dcd4
xiaowu <2457893105@qq.com>
2019-09-05 17:28:59 +0800

    first debug

---
+++ src/main/java/com/onlypiglet/debug/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.onlypiglet.debug.mapper;
+
+import com.onlypiglet.debug.entity.User;
+
+import java.util.List;
+
+/**
+ * @ProjectName: mybatis-3.5.0
+ * @Package: com.onlypiglet.debug.mapper
+ * @ClassName: UserMapper.xml
+ * @Author: 吴成昊
+ * @Description:
+ * @Date: 2019/9/5 16:29
+ * @Version: 0.1
+ */
+public interface UserMapper {
+
+   List<User> selectUsers();
+
+}
